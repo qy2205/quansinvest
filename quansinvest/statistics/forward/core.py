00e3ef0cd894fad7b53bef72176e64e0b1ae52b2
Quan Yuan <quan.yuan@columbia.edu>
2022-08-31 23:18:16 -0400

    add sequential forms

--- quansinvest/statistics/forward/core.py
+++ quansinvest/statistics/forward/core.py
@@ -32,7 +32,11 @@ class ForwardLookStatistics:
             "max_drawdown": max_drawdown,
         }
 
-    def get_results(self, form: AbstractForm, forward_look_period: int) -> list[(pd.DataFrame, dict)]:
+    def get_results(
+        self,
+        form: AbstractForm,
+        forward_look_period: int
+    ) -> list[(pd.DataFrame, pd.DataFrame, dict)]:
         # TODO: parallelize this for loop
         results = []
         start_pos = form.look_back_period - 1
@@ -51,4 +55,37 @@ class ForwardLookStatistics:
 
                 # add to the collected_period_dfs
                 results.append((period_df, return_df, statistics_dict))
+        return results
+
+    def get_sequential_results(
+        self,
+        forms: list[AbstractForm],
+        forward_look_periods: list[int],
+    ) -> list[(list[pd.DataFrame], pd.DataFrame, dict)]:
+        # TODO: parallelize this for loop
+        n_forms = len(forms)
+        results = []
+        start_pos = forms[0].look_back_period - 1
+        end_pos = len(self.data)
+        for cur_pos in range(start_pos, end_pos):
+            period_dfs = []
+            for i, form in enumerate(forms):
+                period_df = self.data.iloc[(cur_pos - form.look_back_period + 1): (cur_pos + 1)]
+                period_dfs.append(period_df)
+                if form.is_form(period_df, cur_pos) and (i + 1) < n_forms:
+                    cur_pos += forward_look_periods[i]
+
+            # period df + forward looking df
+            return_df = self.data.iloc[
+                (cur_pos - forms[-1].look_back_period + 1): (cur_pos + forward_look_periods[-1] + 1)
+            ]
+
+            # calculate statistics
+            if cur_pos + forward_look_periods[-1] + 1 >= end_pos:
+                statistics_dict = {}
+            else:
+                statistics_dict = self._forward_statistics(cur_pos, forward_look_periods[-1])
+
+            # add to the collected_period_dfs
+            results.append((period_dfs, return_df, statistics_dict))
         return results
