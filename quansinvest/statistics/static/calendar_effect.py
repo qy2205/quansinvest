76a41a105ec96e0ec3b4de416bb885680406cc40
Quan Yuan <quan.yuan@columbia.edu>
2022-09-01 23:50:39 -0400

    Update calendar_effect.py
    add return calculation based on different frequency

--- quansinvest/statistics/static/calendar_effect.py
+++ quansinvest/statistics/static/calendar_effect.py
@@ -4,3 +4,22 @@
 3. Christmas and other holidays
 4. weekday performance
 """
+from quansinvest.data.constants import DAILY_RETURN_COLUMN_NAME
+
+
+def get_freq_return(data, freq: str = "month"):
+    def total_return_from_returns(returns):
+        return (returns + 1).prod() - 1
+    return_series = data[DAILY_RETURN_COLUMN_NAME]
+    if freq == "year":
+        return return_series.groupby(
+            by=lambda x: getattr(x, "year")
+        ).apply(total_return_from_returns)
+    elif freq in ["week", "weekday"]:
+        return return_series.groupby(
+            by=lambda x: (getattr(x, "year"), getattr(x.isocalendar(), freq))
+        ).apply(total_return_from_returns)
+    else:
+        return return_series.groupby(
+            by=lambda x: (getattr(x, "year"), getattr(x, freq))
+        ).apply(total_return_from_returns)
